72402530402861a5d3bd6912ee6904052de2e7d9
wayush <107133359+wayushgg@users.noreply.github.com>
2025-11-15 13:37:54 +0530

    Create usage_license.py

---
+++ usage_license.py
@@ -0,0 +1,8 @@
+
+"""
+Arsen Autoclicker is closed source.
+Only the .exe file is provided; the source code is not available.
+All rights reserved. Unauthorized access, distribution, or modification of the source code is prohibited.
+
+For permission requests and contact, please email: brotabsdev@gmail.com
+"""
